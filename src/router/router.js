f0a8ece510b7d38c0185be8b069b1ef55403711c
liupeng <994270241@qq.com>
2019-12-13 14:06:08 +0800

    登录模块-整合路由

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,18 @@
 // 导入Vue
 import Vue from 'vue'
+// 导入 login 组件 
+import login from "../views/login/login.vue"
 // 导包
 import VueRouter from 'vue-router'
 // Use一下 注册
 Vue.use(VueRouter)
 // 规则
-const routes = []
+const routes = [
+    {
+        path : '/login',
+        component : login
+    }
+]
 // 创建
 const router = new VueRouter({
     routes// routes:routes
