d3ad4a2409bc01fd968543a1e8fcd8d83f52ac7c
liupeng <994270241@qq.com>
2019-12-13 17:14:24 +0800

    登陆模块 - 标题区域布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="login-box">
-    <div class="from-box"></div>
+    <!-- 表单盒子 -->
+    <div class="from-box">
+      <!-- 标题 -->
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" alt />
+        <span class="title">黑马面面</span>
+        <i></i>
+        <span class="sub-title">用户登录</span>
+      </div>
+    </div>
+    <!-- 右边图片 -->
     <img class="login-src" src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
@@ -31,6 +41,31 @@ export default {
     width: 478px;
     height: 550px;
     background-color: #f5f5f5;
+
+    padding: 44px;
+    .title-box {
+      display: flex;
+      align-items: center;
+      img {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+      .title {
+        font-size: 24px;
+      }
+      .sub-title {
+        font-size: 22px;
+      }
+      i {
+        display: block;
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-right: 12px;
+        margin-left: 14px;
+      }
+    }
   }
 }
 </style>
