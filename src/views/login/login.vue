9ab53989cdab5d7686b2634e3b492481ddd174ba
liupeng <994270241@qq.com>
2019-12-14 17:29:40 +0800

    登录模块 - 整合表单验证

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -10,13 +10,13 @@
         <span class="sub-title">用户登录</span>
       </div>
       <!-- 饿了么表单布局 -->
-      <el-form ref="form" :model="form">
+      <el-form ref="form" :model="form" :rules="rules">
         <!-- 手机号 -->
-        <el-form-item>
+        <el-form-item prop="phone">
           <el-input placeholder="请输入手机号" v-model="form.phone" prefix-icon="el-icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             placeholder="请输入密码"
             v-model="form.password"
@@ -25,7 +25,7 @@
           ></el-input>
         </el-form-item>
         <!-- 验证码 -->
-        <el-form-item>
+        <el-form-item prop="YZM">
           <el-row>
             <el-col :span="18">
               <el-input placeholder="请输入验证码" v-model="form.YZM" prefix-icon="el-icon-key"></el-input>
@@ -37,7 +37,7 @@
         </el-form-item>
 
         <el-form-item>
-          <el-checkbox-group v-model="form.type">
+          <el-checkbox-group v-model="form.checked">
             <el-checkbox name="type">
               我已阅读并同意
               <el-link type="primary">用户协议</el-link>和
@@ -48,7 +48,7 @@
 
         <!-- 按钮 -->
         <el-form-item>
-          <el-button type="primary" class="login-btn">登录</el-button>
+          <el-button type="primary" class="login-btn" @click="submitForm">登录</el-button>
           <el-button class="register-btn">注册</el-button>
         </el-form-item>
       </el-form>
@@ -62,24 +62,89 @@
 export default {
   name: "login",
   data() {
-    return {
-      form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: "",
-        // 手机
-        phone : '',
-        // 密码
-        password : '',
-        // 验证码
-        YZM : '',
+    // 自定义校验规则的函数
+    var checkphone = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("手机不能为空"));
+      } else {
+        // 利用判断手机号码格式
+        const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+        // 判断是否符合
+        if (reg.test(value) == true) {
+          callback();
+        } else {
+          // 不满足 手机号码的格式
+          callback(new Error("手机号码输入错误"));
+        }
       }
     };
+
+    return {
+      // 表单数据
+      form: {
+        // 手机
+        phone: "",
+        // 密码
+        password: "",
+        // 验证码
+        YZM: "",
+        // 勾选
+        checked: false
+      },
+      // 验证规则
+      rules: {
+        // 手机号
+        phone: [{ required: true, validator: checkphone, trigger: "blur" }],
+        // 密码
+        password: [
+          {
+            required: true,
+            message: "密码不能为空",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            max: 18,
+            message: "密码长度为 6 到 18",
+            trigger: "change"
+          }
+        ],
+        // 验证码
+        YZM: [
+          {
+            required: true,
+            message: "验证码不能为空",
+            trigger: "blur"
+          },
+          {
+            min: 4,
+            max: 4,
+            message: "验证码长度为4",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      if (this.form.checked === false) {
+        // 没勾
+        this.$message.warning("没有勾,先勾一下呗");
+      } else {
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            // 验证成功
+            this.$message.success("恭喜你,成功了");
+          } else {
+            // 验证失败
+            this.$message.error("很遗憾,内容没有写对!");
+
+            return false;
+          }
+        });
+      }
+    }
   }
 };
 </script>
