7c410baf2fad3d5d31ccba3e4cc2efeeb0cb0a4a
liupeng <994270241@qq.com>
2019-12-16 22:05:34 +0800

    首页模块 - 整体布局

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>我是index</div>
+<!-- 布局容器 -->
+  <el-container class="index-container">
+  <el-header class="my-header">Header</el-header>
+  <el-container>
+    <el-aside width="200px" class="my-aside">Aside</el-aside>
+    <el-container>
+      <el-main class="my-main">Main</el-main>
+      
+    </el-container>
+  </el-container>
+</el-container>
 </template>
 
 <script>
@@ -8,6 +18,17 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+    .index-container{
+        height: 100%;
+        .my-header{
+            background-color: skyblue;
+        }
+        .my-aside {
+            background-color: purple;
+        }
+        .my-main{
+            background-color: pink
+        }
+    }
 </style>
